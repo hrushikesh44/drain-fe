a4aa6e00433769ef15f78951ea175aa72c0334d9
hrushikesh44 <hrushikeshmodupalli12345@gmail.com>
2025-02-19 04:54:13 +0530

    modal

--- src/components/CreateContentModal.tsx
+++ src/components/CreateContentModal.tsx
@@ -11,14 +11,16 @@ export function CreateContentModal({ open, onClose }: contentModalProps) {
   return (
     <div>
       {open && (
-        <div className="bg-gray-900 opacity-70 w-screen h-screen fixed flex justify-center ">
-          <div className="flex items-center">
+        <div className="bg-gray-900 opacity-70 w-screen h-screen fixed flex justify-center z-50 ">
+          <div className="flex items-center backdrop-opacity-0 ">
             <span className=" bg-white p-5 rounded-md ">
               <div
                 className="flex justify-between pr-1 cursor-pointer"
                 onClick={onClose}
               >
-                <p className="font-medium text-xl text-black">Add Content</p>
+                <p className="font-medium text-xl text-black pl-2">
+                  Add Content
+                </p>
                 <CloseIcon size={"size-6"} />
               </div>
               <div>
