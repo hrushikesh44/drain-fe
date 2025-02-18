10885a91982a0768b9ca6406a89167e010c94cf2
hrushikesh44 <hrushikeshmodupalli12345@gmail.com>
2025-02-18 05:30:23 +0530

    card

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
-import { Button } from "./components/Button"
-import { PlusIcon } from "./icons/PlusIcon"
-import { ShareIcon } from "./icons/ShareIcon"
+import { Button } from "./components/Button";
+import { Card } from "./components/Card";
+import { PlusIcon } from "./icons/PlusIcon";
+import { ShareIcon } from "./icons/ShareIcon";
 
 function App() {
-  return <div>
-    <div className="flex justify-between mt-11 pl-[20vw] ">
-      <div>
-        <p className="font-bold text-3xl  ">All Notes</p>
+  return (
+    <div>
+      <div className="flex justify-between mt-11 pl-[20vw] ">
+        <div>
+          <p className="font-bold text-3xl  ">All Notes</p>
+        </div>
+        <div className="flex gap-5 mr-5">
+          <Button
+            variant="secondary"
+            text="Share Brain"
+            startIcon={<ShareIcon />}
+          />
+          <Button
+            variant="primary"
+            text="Add Content"
+            startIcon={<PlusIcon />}
+          />
+        </div>
       </div>
-      <div className="flex gap-5 mr-5">
-        <Button variant="secondary" text="Share Brain" startIcon= { <ShareIcon /> } />
-        <Button variant="primary" text="Add Content" startIcon= { <PlusIcon /> } />
+      <div className="flex gap-4 pt-10">
+        <Card
+          title="Buttler with warriors"
+          link="https://youtu.be/2JG26c2uJ7k?si=42_beJNiaL2UZfoU"
+          type="youtube"
+        />
+        <Card
+          title="My Tweet"
+          link="https://x.com/KDTrey5/status/1891541979504222218"
+          type="twitter"
+        />
       </div>
     </div>
-  </div> 
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,55 @@
+import ReactPlayer from "react-player";
+import { DeleteIcon } from "../icons/DeleteIcon";
+import { FileIcon } from "../icons/FileIcon";
+import { ShareIcon } from "../icons/ShareIcon";
+
+interface CardProps {
+  title: string;
+  link: string;
+  type: "youtube" | "twitter";
+}
+
+export function Card({ title, link, type }: CardProps) {
+  return (
+    <div>
+      <div className="p-4 bg-white max-w-96  border border-slate-300 rounded-md shadow-md">
+        <div className="flex justify-between ">
+          <div className="flex ">
+            <div className="text-gray-500 pr-3">
+              <FileIcon />
+            </div>
+            <div className="font-medium text-lg">{title}</div>
+          </div>
+          <div className="flex gap-2 pr-3 items-center">
+            <div className="text-gray-500">
+              <a
+                href={link}
+                target="_blank"
+              >
+                <ShareIcon />
+              </a>
+            </div>
+            <div className="text-gray-500">
+              <DeleteIcon />
+            </div>
+          </div>
+        </div>
+        <div className="pt-4 ">
+          {type === "youtube" && (
+            <ReactPlayer
+              width={348}
+              height={250}
+              url={link}
+            />
+          )}
+
+          {type === "twitter" && (
+            <blockquote className="twitter-tweet">
+              <a href={link.replace("x.com", "twitter.com")}></a>
+            </blockquote>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
